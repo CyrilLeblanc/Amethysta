8233973d36c45804a0d7e02a7c06ec19a16a1b6e
Cyril Leblanc <cyril_leblanc@outlook.fr>
2022-05-04 21:04:40 +0200

    [login | register] rename  `isConnected` function by `isLoggedIn`

--- helpers/user.helper.js
+++ helpers/user.helper.js
@@ -1,16 +1,16 @@
-var UserModel = require('../models/user.model');
+var UserModel = require("../models/user.model");
 
 module.exports = {
-    isConnected: function (req) {
+    isLoggedIn: function (req) {
         return !!req.session.id_user;
     },
-    getUser: async function(req) {
-        if (this.isConnected(req)) {
-            var UserModel = require('../models/user.model');
+
+    getUser: async function (req) {
+        if (this.isLoggedIn(req)) {
             var user = await UserModel.find(req.session.id_user);
             return user;
         } else {
             return null;
         }
-    }
-}
+    },
+};
