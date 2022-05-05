2f8f716012ef86b065cdec5c773b4652b18dd78e
Quentin Villanova <villanovaq@gmail.com>
2022-05-05 09:43:25 +0200

    [feed]W.I.P Feed css

--- routes/feed.router.js
+++ routes/feed.router.js
@@ -3,5 +3,5 @@ const feedController = require("../controllers/feed.controller");
 
 const feedRouter = express.Router();
 feedRouter.get("/json", feedController.getFeed);
-feedRouter.get("/page", feedController.getFeedPage);
+feedRouter.get("/", feedController.getFeedPage);
 module.exports = feedRouter;
