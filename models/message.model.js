58304bf247aad633b54e4c352f3c3aa72683ba20
Cyril Leblanc <cyril_leblanc@outlook.fr>
2022-05-06 11:47:41 +0200

    [message] implementing message feature

--- models/message.model.js
+++ models/message.model.js
@@ -1,6 +1,27 @@
 const ConversationModel = require("./conversation.model");
-var MessageModel = require("./repository");
+const mysql = require("./mysql");
+var MessageModel = require("./repository").init("message");
 
-MessageModel.getByConversation = async function (conversation) {};
-MessageModel.create = async function (conversation, user) {};
+MessageModel.getByConversation = function (conversation) {
+    return new Promise((resolve, reject) => {
+        mysql.execute(
+            "SELECT * FROM message WHERE id_conversation = ? ORDER BY date ASC LIMIT 100",
+            [conversation.id_conversation],
+            (err, results) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(results);
+            }
+        );
+    });
+};
+MessageModel.create = async function (conversation, user, content) {
+    return await this.insert({
+        id_conversation: conversation.id_conversation,
+        id_user: user.id_user,
+        content: content,
+        date: new Date(),
+    })
+};
 module.exports = MessageModel;
