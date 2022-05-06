ab9d2bb09ec2816b18a31a0db20d684a34016d1e
Cyril Leblanc <cyril_leblanc@outlook.fr>
2022-05-06 15:40:30 +0200

    [master] remove console.log

--- controllers/feed.controller.js
+++ controllers/feed.controller.js
@@ -4,18 +4,13 @@ const modelUser = require("../models/user.model");
 module.exports = {
     getFeedPage: async function (req, res, next) {
         var allPost = await modelPost.findAll();
-        console.log("chouchou");
-        console.log(allPost);
 
         allPost.forEach(async function(chat){
                 chat.id_user = await modelUser.getById(chat.id_user);
         })
-        console.log(allPost.lenght + " taille");
         for (let i = 0; i < allPost.lenght; i++) {
 
         }
-        console.log(typeof allPost + " type");
-        console.log(allPost);
         res.render("base", {
             template: 'feed',
             title: "Feed",
