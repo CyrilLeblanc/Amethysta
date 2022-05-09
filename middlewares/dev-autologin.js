9d6c50d628f1441fb57f9b868eefd686de9b2950
Quentin Villanova <villanovaq@gmail.com>
2022-05-09 09:41:37 +0200

    [feed]More profile form styling

--- middlewares/dev-autologin.js
+++ middlewares/dev-autologin.js
@@ -7,7 +7,7 @@ module.exports = function(req, res, next) {
     const ENVIRONMENT = process.env.ENVIRONMENT;
     const ID_DEV_USER= process.env.ID_DEV_USER;
     if (ENVIRONMENT === "dev" && ID_DEV_USER) {
-        req.session.id_user = ID_DEV_USER;
+        //req.session.id_user = ID_DEV_USER;
     }
     next();
 }
